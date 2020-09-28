82c21ba1f128b9df0cec4ab6d4c5176a586c4a17
Nathan2401 <oluwolefalasinu@gmail.com>
2020-09-28 20:15:45 +0100

    add new files to the translate folder

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+
+const items = [
+  {
+    title: "What is React ?",
+    content: "React is a Javascript framework"
+  },
+
+  {
+    title: "Why use React ?",
+    content: "React is the favorite JavaScript library for engineers"
+  },
+
+  {
+    title: "How do you use React ?",
+    content: "You use React by creating components "
+  }
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red"
+  },
+  {
+    label: "The Color Green",
+    value: "green"
+  },
+  {
+    label: "A shade of Blue",
+    value: "blue"
+  }
+];
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div>
+      <Translate />
+    </div>
+  );
+};
+
+//<Accordion items={items}/>
